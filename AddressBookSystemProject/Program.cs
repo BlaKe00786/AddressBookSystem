ee2ac30f871553ff891b808f29a4e0259758fdf2
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-02 20:05:11 +0530

    [Sameer]Add: Added a program file to display Welcome message in master branch

---
+++ AddressBookSystemProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookSystemProject
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System");
+        }
+    }
+}
