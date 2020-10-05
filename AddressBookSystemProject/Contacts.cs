432aefe64b73683b2b1a0dd2e2d0161bf0b25aa2
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-05 22:21:14 +0530

    [Sameer]Refactor: Proper Implmentation with minor changes.

--- AddressBookSystemProject/Contacts.cs
+++ AddressBookSystemProject/Contacts.cs
@@ -26,11 +26,4 @@ namespace AddressBookSystemProject
             this.Email = Email;
         }
     }
-    public interface IContacts
-    {
-        public void AddContact(string FirstName, string LastName, string Address, string City, string State, string Zip, string PhoneNumber, string Email);
-        public void DisplayContacts();
-        public void EditContact(string FirstName);
-        public void DeleteContact(string FirstName);
-    }
 }
